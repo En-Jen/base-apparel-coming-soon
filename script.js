461635b4682a1120d721dc59abeb5d1bcdf6b6e7
Jen Speak <jspeak6@gmail.com>
2020-09-14 14:31:37 -0700

    Added successful subscribtion feedback

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const form = document.querySelector('.form');
-const email = document.querySelector('input[type=email]');
+const emailInput = document.querySelector('input[type=email]');
+const feedback = document.querySelector('.form__feedback');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    let emailValue = email.value;
-    console.log(email.value);
-    if (validateEmail(emailValue)) {
+
+    // Check if valid email is submitted
+    if (validateEmail(emailInput.value)) {
         form.classList.remove('error');
+        form.classList.add('success');
+        feedback.textContent = 'You have been subscribed';
     } else {
+        form.classList.remove('success');
         form.classList.add('error');
+        feedback.textContent = 'Please provide a valid email';
     }
 });
 
+// Regular expression to validate email
 function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
